edd593d434b1105cfa12bc89002fe18ef28b5092
Anna Gabain <anna.gabain@outlook.com>
2022-05-09 18:31:09 +0000

    new commands

--- commands.sh
+++ commands.sh
@@ -14,3 +14,16 @@ clear
 
 # Close the terminal
 exit
+
+# Get the current location (print working directory)
+pwd
+
+# List all of the files in current directory
+ls
+
+# Change directory (go up one)
+cd ..
+
+# Change into the workspace directory (if it exists inside of the current working directory)
+cd workspace
+
